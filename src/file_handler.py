db72d697ea0189f69d138fad4bce37522eca00ad
EthanLaPointe <e.lapointe@comcast.net>
2026-05-21 17:59:09 -0400

    Pep8 formatting and documentation finished for all functions

--- src/file_handler.py
+++ src/file_handler.py
@@ -13,11 +13,30 @@ import pandas as pd
 
 from report import Report
 from src.data_cache import DataCache
-from src.db_connection import *
+from src.db_connection import (
+    CustomerAlias,
+    CustomerLocation,
+    DAOFactory,
+    Item,
+    Location,
+    ReportLine,
+    SaleCustomer,
+    SalesReport,
+)
 
 # List of fields a report must contain
-REPORT_FIELD_LIST = ["customername", "city", "state", "stockcode", "productfam", "productdesc", "quantity", "saledate",
-                     "amount", "transfer"]
+REPORT_FIELD_LIST = [
+    "customername",
+    "city",
+    "state",
+    "stockcode",
+    "productfam",
+    "productdesc",
+    "quantity",
+    "saledate",
+    "amount",
+    "transfer",
+]
 
 # List of fields an alias mapping list must contain
 MAPPING_FIELD_LIST = ["alias", "parent"]
@@ -26,26 +45,55 @@ MAPPING_FIELD_LIST = ["alias", "parent"]
 MAPPING_CODES = {"valid": 0, "duplicate alias": 1, "unknown customer": 2}
 
 class FileHandler:
+    """To be finished later."""
 
-    def __init__(self, dao_factory: DAOFactory, data_cache: DataCache):
+    def __init__(self, dao_factory: DAOFactory, data_cache: DataCache) -> None:
+        """Initialize a FileHandler instance.
+
+        Args:
+            dao_factory (DAOFactory):
+                The DAOFactory the handler should use.
+            data_cache (DataCache):
+                The DataCache of the main app.
+
+        """
         self.dao = dao_factory
         self.cache = data_cache
 
     def insert_report(self, report: Report) -> None:
-        # dict to store row information
-        # column index for row matches line_data, i.e. line_data["customer"] = row[0]
-        line_data = {"customername": '', "city": '', "state": '', "stockcode": '', "productfamily": '', "productdesc": '',
-                     "quantity": 0, "saledate": None, "amount": 0.0, "transfer": ''}
+        """Insert a sales report into the database.
 
-        # Use report manufacturer name to get manufacturer DAO
+        Args:
+            report (Report):
+                The Report object containing the sales report information.
+
+        """
+        line_data = {
+            "customername": "",
+            "city": "",
+            "state": "",
+            "stockcode": "",
+            "productfamily": "",
+            "productdesc": "",
+            "quantity": 0,
+            "saledate": None,
+            "amount": 0.0,
+            "transfer": "",
+        }
+
+        # Use report manufacturer name to get manufacturer from dao
         manufacturer = self.dao.manufacturers.get_by_name(report.manufacturerName)
-            
+
         try:
-            sales_report = SalesReport(report_id= None, manufacturer_id= manufacturer.manufacturer_id, report_year= report.year, report_month= report.month)
+            sales_report = SalesReport(
+                report_id=None,
+                manufacturer_id=manufacturer.manufacturer_id,
+                report_year=report.year,
+                report_month=report.month,
+            )
             sales_report = self.dao.sales_reports.create(sales_report)
-                
-        except psycopg2.Error as insert_exception:
-            print("Failed to insert sales report. Error: ", insert_exception)
+
+        except Exception: # noqa: BLE001
             return
 
         sale_customer_list = []
@@ -72,29 +120,63 @@ class FileHandler:
 
             # insert location
             if (line_data["city"], line_data["state"]) in self.cache.locations:
-                location_id = self.cache.locations[(line_data["city"], line_data["state"])]
+                location_id = self.cache.locations[(
+                    line_data["city"],
+                    line_data["state"],
+                )]
             else:
-                location_id = self.dao.locations.create(Location(location_id = None, city = line_data["city"], state = line_data["state"])).location_id
-                self.cache.locations[(line_data["city"], line_data["state"])] = location_id
+                location_id = self.dao.locations.create(
+                    Location(
+                        location_id=None,
+                        city=line_data["city"],
+                        state=line_data["state"],
+                    ),
+                ).location_id
+                self.cache.locations[(line_data["city"], line_data["state"])] \
+                = location_id
 
             # save each customer location to a list of CustomerLocation
-            customer_location = CustomerLocation(customer_id = customer_id, location_id = location_id)
+            customer_location = CustomerLocation(
+                customer_id=customer_id,
+                location_id=location_id,
+            )
             customer_location_list.append(customer_location)
 
             # save each sale customer to a list of SaleCustomer
-            sale_customer = SaleCustomer(customer_id = customer_id, location_id = location_id, report_id = sales_report.report_id)
+            sale_customer = SaleCustomer(
+                customer_id=customer_id,
+                location_id=location_id,
+                report_id=sales_report.report_id,
+            )
             sale_customer_list.append(sale_customer)
 
             # insert item
             if line_data["stockcode"] in self.cache.items:
                 item_id = self.cache.items[line_data["stockcode"]]
             else:
-                item_id = self.dao.items.create(Item(item_id = None, stockcode = line_data["stockcode"], product_family = line_data["productfamily"], product_description = line_data["productdesc"])).item_id
+                item_id = self.dao.items.create(
+                    Item(
+                        item_id=None,
+                        stockcode=line_data["stockcode"],
+                        product_family=line_data["productfamily"],
+                        product_description=line_data["productdesc"],
+                    ),
+                ).item_id
                 self.cache.items[line_data["stockcode"]] = item_id
 
             # save each line of report to a list of ReportLine
-            line = ReportLine(report_line_id = None, report_id = sales_report.report_id, customer_alias= line_data["customername"], customer_id = customer_id, item_id = item_id, location_id = location_id, quantity = line_data["quantity"],
-                              transfer = line_data["transfer"], amt = Decimal(line_data["amount"]), sale_date = line_data["saledate"] )
+            line = ReportLine(
+                report_line_id=None,
+                report_id=sales_report.report_id,
+                customer_alias=line_data["customername"],
+                customer_id=customer_id,
+                item_id=item_id,
+                location_id=location_id,
+                quantity=line_data["quantity"],
+                transfer = line_data["transfer"],
+                amt = Decimal(line_data["amount"]),
+                sale_date = line_data["saledate"],
+            )
             report_line_list.append(line)
 
         # insert data from lists
@@ -102,11 +184,20 @@ class FileHandler:
         self.dao.sale_customers.create_bulk(sale_customer_list)
         self.dao.report_lines.create_bulk(report_line_list)
 
-    # TODO look into changing to use dataframes header row instead of reading file a second time
-    def trim_report(self, dataframe, file_path) -> pd.DataFrame:
+    # look into changing to use dataframes header row instead of reading file
+    def trim_report(self, dataframe: pd.DataFrame, file_path: str) -> pd.DataFrame:
+        """Trim a report to contain only desired fields.
+
+        Args:
+            dataframe (Pandas.DataFrame):
+                The dataframe of the report to trim.
+            file_path (str):
+                The filepath of the report .csv.
+
+        """
         fields_to_keep = []
-        with open(file_path) as csv_file:
-            csv_reader = csv.reader(csv_file, delimiter=',')
+        with Path.open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",")
             header = next(csv_reader)
 
         #Check column numbers for each desired field
@@ -117,10 +208,10 @@ class FileHandler:
                     break
 
         #Trim DF to only contain desired columns
-        dataframe = dataframe.iloc[:, fields_to_keep]
-        return dataframe
+        return dataframe.iloc[:, fields_to_keep]
 
-    def fill_empty(self, dataframe) -> pd.DataFrame:
+    def fill_empty(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        """Fill empty rows and add any missing dataframe columns."""
         #Loop through desired fields
         header_locations = []
         for field in REPORT_FIELD_LIST:
@@ -129,37 +220,81 @@ class FileHandler:
             for idx, col in enumerate(dataframe.columns):
                 #If present add fields location to location list
                 if field in col.lower():
-                    #print("found: ", field)
                     header_locations.append(idx)
                     field_found = True
                     break
-            #If desired field is not found insert it next to the most recently found field
+            # If desired field is not found insert it
+            # next to the most recently found field
             if not field_found:
-                #print("inserting: ", field)
-                dataframe.insert(loc=header_locations[-1] + 1, column=field, value=np.nan)
+                dataframe.insert(
+                    loc=header_locations[-1] + 1,
+                    column=field,
+                    value=np.nan,
+                )
         #Update column names to match field list
         dataframe.columns = REPORT_FIELD_LIST
         dataframe = dataframe.map(lambda s: s.lower() if isinstance(s, str) else s)
         #Change instances of nan to proper datatypes in each col
-        dataframe["quantity"] = dataframe["quantity"].fillna(0).astype(str).str.replace(r'[$,)#]', '', regex=True).str.strip().astype(float).astype(int)
+        dataframe["quantity"] = dataframe["quantity"].fillna(0).astype(str).str.replace(
+            r"[$,)#]",
+            "",
+            regex=True,
+        ).str.strip().astype(float).astype(int)
         dataframe["saledate"] = dataframe["saledate"].fillna(None)
         #Strip whitespace from city, state
         dataframe["city"] = dataframe["city"].astype(str).str.strip()
         dataframe["state"] = dataframe["state"].astype(str).str.strip()
         #Remove any $ from amount column
-        dataframe["amount"] = dataframe["amount"].astype(str).str.replace(r'[$,)#]', '', regex=True).str.strip().replace('', '0.0').replace(r'[-(]', '-0', regex=True).fillna(0.0).astype(float)
+        dataframe["amount"] = dataframe["amount"].astype(str).str.replace(
+            r"[$,)#]",
+            "",
+            regex=True,
+        ).str.strip().replace(
+            "",
+            "0.0",
+        ).replace(
+            r"[-(]",
+            "-0",
+            regex=True,
+        ).fillna(0.0).astype(float)
         #Remove special characters from customer name
-        dataframe["customername"] = dataframe["customername"].astype(str).str.replace(r"[.'(),-]", '', regex=True).str.replace(r' +', ' ', regex=True).str.strip()
+        dataframe["customername"] = dataframe["customername"].astype(str).str.replace(
+            r"[.'(),-]",
+            "",
+            regex=True,
+        ).str.replace(r" +", " ", regex=True).str.strip()
         #Fill any leftover empty cells with None
         dataframe = dataframe.replace({np.nan: None})
-        #Remove any rows where amount is 0 & set quantity to null if it = 0 where amount is > 0
+        #Remove any row where amount is 0
+        #And set quantity to null if it = 0 where amount is > 0
         dataframe = dataframe[dataframe["amount"] != 0.0]
-        dataframe["quantity"] = np.where((dataframe["amount"] > 0) & (dataframe["quantity"] == 0), None, dataframe["quantity"])
-        
+        dataframe["quantity"] = (
+            np.where((dataframe["amount"] > 0)
+            & (dataframe["quantity"] == 0),
+            None,
+            dataframe["quantity"]),
+        )
+
         return dataframe
-    
+
     def standardize_report(self, report: Report) -> Report:
-        report.dataframe = self.fill_empty(self.trim_report(report.dataframe, report.filePath))
+        """Trim and clean a Report object.
+
+        Args:
+            report (Report):
+                The report object containing the sales report information.
+
+        Returns:
+            Report:
+                A Report object with the standardized dataframe.
+
+        """
+        report.dataframe = self.fill_empty(
+            self.trim_report(
+                report.dataframe,
+                report.filePath,
+            ),
+        )
         return report
 
     #------------------------------------------------------------------
@@ -167,16 +302,32 @@ class FileHandler:
     #------------------------------------------------------------------
 
     def check_report(self, report: Report) -> tuple[tuple[bool, bool], dict[str, int]]:
+        """Check if a report is valid to be inserted.
+
+        Args:
+            report (Report):
+                The report to check validity of.
+
+        Returns:
+            tuple[tuple[bool, bool], dict[str, int]]:
+                A tuple containing a pair of bools(valid_manufacturer, already_present)
+                and a dictionary containing all unknown customer aliases in the report.
+
+        """
         unknown_list = {}
         valid_manufacturer = False
         already_present = False
-        manufacturer_id = self.cache.manufacturers[report.manufacturerName] if (report.manufacturerName in self.cache.manufacturers) else None
+        manufacturer_id = self.cache.manufacturers.get(report.manufacturerName, None)
 
         # check if manufacturer on report exists in database
-        # if exists then check if a report already exists for that manufacturer during the report period
+        # if exists then check if a report already exists
+        # for that manufacturer during the report period
         if manufacturer_id is not None:
             valid_manufacturer = True
-            already_present = self.dao.sales_reports.check_exists(manufacturer_id, report.year,report.month)
+            already_present = self.dao.sales_reports.check_exists(
+                manufacturer_id,
+                report.year,report.month,
+            )
 
         # Add each unique unknown name to the unknown_list
         for name in report.dataframe["customername"]:
@@ -184,17 +335,37 @@ class FileHandler:
                 unknown_list[name] = 0
 
         return (valid_manufacturer, already_present), unknown_list
-    
+
     def read_mappings(self, file_path: str) -> pd.DataFrame:
+        """Read a .csv file of customer alias mappings.
+
+        Args:
+            file_path (str):
+                The file path of the mapping file.
+
+        Returns:
+            Pandas.DataFrame:
+
+        """
         file_path = Path(file_path)
-        mappings = pd.read_csv(file_path, encoding='latin-1').astype(str)
-        return mappings
-    
+        return pd.read_csv(file_path, encoding="latin-1").astype(str)
+
     def check_mappings(self, dataframe: pd.DataFrame) -> tuple[pd.DataFrame, bool]:
-        
+        """Check the validity of a customer alias mapping.
+
+        Args:
+            dataframe (Pandas.DataFrame):
+                The dataframe read from the mapping file.
+
+        Returns:
+            tuple[Pandas.DataFrame, bool]:
+                A tuple containing the trimmed dataframe and True or False
+                depending on the validity of the mappings.
+
+        """
         valid_columns = False
-        column_list = list(map(lambda x: x.lower(), dataframe.columns.tolist()))
-        
+        column_list = [x.lower() for x in dataframe.columns.tolist()]
+
         fields_to_keep = []
 
         #Check column numbers for each desired field
@@ -207,22 +378,45 @@ class FileHandler:
         #Trim DF to only contain desired columns
         dataframe = dataframe.iloc[:, fields_to_keep]
         dataframe = dataframe.dropna()
-        column_list = list(map(lambda x: x.lower(), dataframe.columns.tolist()))
+        column_list = [x.lower() for x in dataframe.columns.tolist()]
 
-        if MAPPING_FIELD_LIST[0] in column_list and MAPPING_FIELD_LIST[1] in column_list:
+        if (
+            MAPPING_FIELD_LIST[0] in column_list
+            and MAPPING_FIELD_LIST[1] in column_list
+        ):
             valid_columns = True
             dataframe.columns = MAPPING_FIELD_LIST
             self.cache.refresh()
-            
+
             dataframe.insert(loc=len(dataframe.columns), column="status", value=np.nan)
-            
+
             # Filter names in alias and parent columns to match report filtering
             dataframe = dataframe.map(lambda s: s.lower() if isinstance(s, str) else s)
-            dataframe["parent"] = dataframe["parent"].astype(str).str.replace(r"[.'(),-]", '', regex=True).str.replace(r' +', ' ', regex=True).str.strip()
-            dataframe["alias"] = dataframe["alias"].astype(str).str.replace(r"[.'(),-]", '', regex=True).str.replace(r' +', ' ', regex=True).str.strip()
-            
+            dataframe["parent"] = dataframe["parent"].astype(str).str.replace(
+                r"[.'(),-]",
+                "",
+                regex=True,
+            ).str.replace(
+                r" +",
+                " ",
+                regex=True,
+            ).str.strip()
+            dataframe["alias"] = dataframe["alias"].astype(str).str.replace(
+                r"[.'(),-]",
+                "",
+                regex=True,
+            ).str.replace(
+                r" +",
+                " ",
+                regex=True,
+            ).str.strip()
+
             # Set row status
-            dataframe["status"] = np.where(dataframe["alias"].isin(self.cache.customer_aliases), MAPPING_CODES["duplicate alias"], MAPPING_CODES["valid"])
+            dataframe["status"] = np.where(
+                dataframe["alias"].isin(self.cache.customer_aliases),
+                MAPPING_CODES["duplicate alias"],
+                MAPPING_CODES["valid"],
+            )
             # Skip any rows that already have a non-valid status
             dataframe["status"] = np.where(
                 dataframe["status"] == MAPPING_CODES["duplicate alias"],
@@ -230,24 +424,33 @@ class FileHandler:
                 np.where(
                     dataframe["parent"].isin(self.cache.customer_aliases),
                     MAPPING_CODES["valid"],
-                    MAPPING_CODES["unknown customer"]
-                )
+                    MAPPING_CODES["unknown customer"],
+                ),
             )
-            
+
         return (dataframe, valid_columns)
-    
+
     def insert_alias_mappings(self, dataframe: pd.DataFrame) -> bool:
+        """Insert customer alias mappings into database.
+
+        Args:
+            dataframe (Pandas.DataFrame):
+                The dataframe of customer alias mappings.
+
+        Returns:
+            bool:
+                True or False depending on insertion success.
+
+        """
         mappings_to_insert = []
         for row in dataframe.itertuples(index=False):
             customer_id = self.cache.customer_aliases[row[1]]
             alias = row[0]
             mapping = CustomerAlias(alias=alias, customer_id=customer_id)
             mappings_to_insert.append(mapping)
-        
-        success = self.dao.customer_aliases.create_bulk(mappings_to_insert)
-        
-        return success
-        
-        
-        
+
+        return self.dao.customer_aliases.create_bulk(mappings_to_insert)
+
+
+
 
